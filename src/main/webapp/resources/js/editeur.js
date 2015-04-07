c4f2cc130060f6a91778976614f2f1804ef07069
Geoffrey Dumas <geoffrey.dumas@etud.univ-montp2.fr>
2015-04-07 19:50:56 +0200

    ADD controller party form

--- src/main/webapp/resources/js/editeur.js
+++ src/main/webapp/resources/js/editeur.js
@@ -1,5 +1,29 @@
-	$(document).ready(function() {
-				$('.my-code-area1,.my-code-area2,.my-code-area3,.my-code-area4,.my-code-area5,.my-code-area6').ace({ theme: 'cobalt', lang: 'python' });
-				$('.ace_editor').css('width','100%');
-				$('.ace_editor').css('height','100%');
-	});
+$(document).ready(function(){
+
+    			var editor1 = ace.edit("editor1");
+			    editor1.setTheme("ace/theme/monokai");
+			    editor1.getSession().setMode("ace/mode/python");
+				var langTools = ace.require("ace/ext/language_tools");
+    			editor1.setOptions({enableBasicAutocompletion: true});
+
+
+			    var editor2 = ace.edit("editor2");
+			    editor2.setTheme("ace/theme/monokai");
+			    editor2.getSession().setMode("ace/mode/python");
+
+			    var editor3 = ace.edit("editor3");
+			    editor3.setTheme("ace/theme/monokai");
+			    editor3.getSession().setMode("ace/mode/python");
+
+			    var editor4 = ace.edit("editor4");
+			    editor4.setTheme("ace/theme/monokai");
+			    editor4.getSession().setMode("ace/mode/python");
+
+			    var editor5 = ace.edit("editor5");
+			    editor5.setTheme("ace/theme/monokai");
+			    editor5.getSession().setMode("ace/mode/python");
+
+			    var editor6 = ace.edit("editor6");
+			    editor6.setTheme("ace/theme/monokai");
+			    editor6.getSession().setMode("ace/mode/python");
+ });
