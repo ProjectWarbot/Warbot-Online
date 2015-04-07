9483959449e9faf1313f3d2a60a8c28a7bd1d658
Geoffrey Dumas <geoffrey.dumas@etud.univ-montp2.fr>
2015-04-07 23:04:19 +0200

    ADD controller party form

--- src/main/webapp/resources/js/editeur.js
+++ src/main/webapp/resources/js/editeur.js
@@ -3,27 +3,31 @@ $(document).ready(function(){
     			var editor1 = ace.edit("editor1");
 			    editor1.setTheme("ace/theme/monokai");
 			    editor1.getSession().setMode("ace/mode/python");
-				var langTools = ace.require("ace/ext/language_tools");
-    			editor1.setOptions({enableBasicAutocompletion: true});
+				editor1.setOptions({enableBasicAutocompletion: true});
 
 
 			    var editor2 = ace.edit("editor2");
 			    editor2.setTheme("ace/theme/monokai");
 			    editor2.getSession().setMode("ace/mode/python");
+			    editor2.setOptions({enableBasicAutocompletion: true});
 
 			    var editor3 = ace.edit("editor3");
 			    editor3.setTheme("ace/theme/monokai");
 			    editor3.getSession().setMode("ace/mode/python");
+			    editor3.setOptions({enableBasicAutocompletion: true});
 
 			    var editor4 = ace.edit("editor4");
 			    editor4.setTheme("ace/theme/monokai");
 			    editor4.getSession().setMode("ace/mode/python");
+			    editor4.setOptions({enableBasicAutocompletion: true});
 
 			    var editor5 = ace.edit("editor5");
 			    editor5.setTheme("ace/theme/monokai");
 			    editor5.getSession().setMode("ace/mode/python");
+			    editor5.setOptions({enableBasicAutocompletion: true});
 
 			    var editor6 = ace.edit("editor6");
 			    editor6.setTheme("ace/theme/monokai");
 			    editor6.getSession().setMode("ace/mode/python");
+			    editor6.setOptions({enableBasicAutocompletion: true});
  });
