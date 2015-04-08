e706b19d42f11e61074839996ee97a3b35469cc9
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-04-08 18:14:58 +0200

    chargement code python par default

--- src/main/java/edu/warbot/controllers/PartyController.java
+++ src/main/java/edu/warbot/controllers/PartyController.java
@@ -1,16 +1,29 @@
 package edu.warbot.controllers;
 
+import edu.warbot.agents.enums.WarAgentType;
 import edu.warbot.form.PartyForm;
 import edu.warbot.models.Account;
 import edu.warbot.models.Party;
+import edu.warbot.models.WebAgent;
+import edu.warbot.models.WebCode;
 import edu.warbot.repository.AccountRepository;
 import edu.warbot.repository.PartyRepository;
+import edu.warbot.repository.WebAgentRepository;
+import edu.warbot.repository.WebCodeRepository;
+import edu.warbot.scriptcore.script.Script;
 import edu.warbot.services.CodeEditorService;
 import edu.warbot.services.TeamService;
 import edu.warbot.support.web.MessageHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
@@ -21,7 +34,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.Principal;
+import java.util.*;
 
 /**
  * Created by BEUGNON on 18/03/2015.
@@ -30,7 +47,7 @@ import java.security.Principal;
  */
 @Controller
 @Secured({"ROLE_USER","ROLE_ADMIN"})
-public class PartyController
+public class PartyController implements ApplicationContextAware
 {
     final Logger logger = LoggerFactory.getLogger(PartyController.class);
 
@@ -41,7 +58,16 @@ public class PartyController
     private AccountRepository accountRepository;
 
     @Autowired
+    private WebAgentRepository webAgentRepository;
+
+    @Autowired
+    private WebCodeRepository webCodeRepository;
+
+    @Autowired
     private CodeEditorService codeEditorService;
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @RequestMapping(value = "party/entity", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
@@ -84,7 +110,41 @@ public class PartyController
 
             //TODO ADD DEFAULT CODE FOR PARTY
 
+            Map<WarAgentType, StringBuilder> codeAgent = new HashMap<>();
 
+            try {
+                Resource[] resources = applicationContext.getResources("classpath:script/python/*");
+
+                if(resources.length == 0)
+                    throw new IOException("Probleme avec ressource python");
+
+                for(Resource resource : resources) {
+                    Scanner scanner = new Scanner(resource.getFile());
+                    StringBuilder sb = new StringBuilder();
+
+                    while(scanner.hasNext())
+                    {
+                        sb.append(scanner.nextLine());
+                        sb.append("\n");
+                    }
+                    scanner.close();
+                    int index = resource.getFilename().indexOf(".");
+                    String typeAgent = resource.getFilename().substring(0, index);
+
+                    codeAgent.put(WarAgentType.valueOf(typeAgent), sb);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            List<WebAgent> webAgents = webAgentRepository.findAllStarter();
+
+            for(WebAgent agent : webAgents) {
+                WebCode webCode = new WebCode(agent, party);
+                webCode.setContent(codeAgent.get(agent.getType()).toString());
+                webCodeRepository.save(webCode);
+            }
         }
         else
         {
@@ -109,4 +169,9 @@ public class PartyController
     }
 
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+
+    }
 }
