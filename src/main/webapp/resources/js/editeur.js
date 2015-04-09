9555d9cff1f7d505e64aba233d7d214a353137f1
TsubameDono <sebastien.beugnon.pro@gmail.com>
2015-04-09 18:44:45 +0200

    Loading by websocket of webcode by agent and by party, fix bug with double stomp connect

--- src/main/webapp/resources/js/editeur.js
+++ src/main/webapp/resources/js/editeur.js
@@ -1,3 +1,4 @@
+var editors ={};
 $(document).ready(function(){
 
 				$(".editor").each(function( index )
@@ -10,6 +11,7 @@ $(document).ready(function(){
                     ed.getSession().setMode("ace/mode/python");
                     ed.setOptions({enableBasicAutocompletion: true});
 
+                    editors[id] = ed;
                     this.style.position = "absolute";
                     this.style.marginTop = "42px";
                     this.style.top = 0;
@@ -21,80 +23,4 @@ $(document).ready(function(){
                    	$(this).html += "def toto :\n\t pass";
 
 				});
-
-
-
-//    			var editor1 = ace.edit("editor1");
-//
-//
-//			    var editor2 = ace.edit("editor2");
-//			    editor2.setTheme("ace/theme/monokai");
-//			    editor2.getSession().setMode("ace/mode/python");
-//			    editor2.setOptions({enableBasicAutocompletion: true});
-//
-//			    var editor3 = ace.edit("editor3");
-//			    editor3.setTheme("ace/theme/monokai");
-//			    editor3.getSession().setMode("ace/mode/python");
-//			    editor3.setOptions({enableBasicAutocompletion: true});
-//
-//			    var editor4 = ace.edit("editor4");
-//			    editor4.setTheme("ace/theme/monokai");
-//			    editor4.getSession().setMode("ace/mode/python");
-//			    editor4.setOptions({enableBasicAutocompletion: true});
-//
-//			    var editor5 = ace.edit("editor5");
-//			    editor5.setTheme("ace/theme/monokai");
-//			    editor5.getSession().setMode("ace/mode/python");
-//			    editor5.setOptions({enableBasicAutocompletion: true});
-//
-//			    var editor6 = ace.edit("editor6");
-//			    editor6.setTheme("ace/theme/monokai");
-//			    editor6.getSession().setMode("ace/mode/python");
-//			    editor6.setOptions({enableBasicAutocompletion: true});
  });
-
-
-function EditorModel(stompClient) {
-  var self = this;
-
-  self.connect = function() {
-      stompClient.connect({}, function(frame) {
-
-      console.log('Connected ' + frame);
-
-      stompClient.subscribe("/app/editor/register", function(message) {
-        /* Return a message if can save */
-        // NOT NEED
-      });
-
-      stompClient.subscribe("/user/editor/code",function (message)
-      {
-        var m = JSON.parse(message.body);
-
-        $("#"+m.idWebAgent+"-editor").append(m.content);
-      });
-
-    }, function(error) {
-      console.log("STOMP protocol error " + error);
-    });
-  }
-
-  self.get = function(idParty1,idAgent1) {
-      var trade = {
-          "idParty" : idParty1,
-          "idWebAgent" : idAgent1
-      };
-
-      stompClient.send("/app/editor/get",{},JSON.stringify(trade));
-    }
-
-  self.save = function(idParty1,idAgent1,content) {
-  	var trade = {
-  	 	"idParty" : idParty1,
-    	"idWebAgent" : idAgent1,
-        "content" : content
-    };
-    stompClient.send("/app/editor/save", {}, JSON.stringify(trade));
-  }
-
-}
--- src/main/webapp/resources/js/webgame.js
+++ src/main/webapp/resources/js/webgame.js
@@ -30,6 +30,28 @@ function WebGameModel(stompClient) {
       stompClient.subscribe("/user/queue/errors", function(message) {
         self.pushNotification("Error " + message.body);
       });
+
+      stompClient.subscribe("/app/editor/register", function(message) {
+              /* Return a message if can save */
+              // NOT NEED
+            });
+
+            stompClient.subscribe("/user/editor/code",function (message)
+            {
+              var m = JSON.parse(message.body);
+              var editor = $("#"+m.webAgentId+"-editor");
+
+              if (editor.length==0)
+              {
+                  console.log("none");
+              }
+              else
+              {
+                  console.log(editors);
+                  editors[m.webAgentId+"-editor"].getSession().setValue(m.content);
+              }
+
+            });
     }, function(error) {
       console.log("STOMP protocol error " + error);
     });
@@ -42,12 +64,30 @@ function WebGameModel(stompClient) {
         };
       console.log(trade);
       stompClient.send("/app/game/start", {}, JSON.stringify(trade));
-    }
+    };
+
+     self.get = function(idParty1,idAgent1) {
+          var trade = {
+              "idParty" : idParty1,
+              "idWebAgent" : idAgent1
+          };
+
+          stompClient.send("/app/editor/get",{},JSON.stringify(trade));
+        };
+
+      self.save = function(idParty1,idAgent1,content) {
+      	var trade = {
+      	 	"idParty" : idParty1,
+        	"idWebAgent" : idAgent1,
+            "content" : content
+        };
+        stompClient.send("/app/editor/save", {}, JSON.stringify(trade));
+      }
 
   self.pushNotification = function(text) {
     self.notifications.push({notification: text});
     if (self.notifications().length > 5) {
       self.notifications.shift();
     }
-  }
+  };
 }
