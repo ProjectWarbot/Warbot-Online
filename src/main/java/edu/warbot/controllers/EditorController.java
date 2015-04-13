bb45a7e36cc21b1bab6cf2ae3427e1b2d4e1c9ab
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-04-13 20:30:20 +0200

    Debut de l'editeur de configuration, ajout Controller java + onglet Header + page html

---
+++ src/main/java/edu/warbot/controllers/EditorController.java
@@ -0,0 +1,28 @@
+package edu.warbot.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by jimmy on 13/04/15.
+ */
+
+@Controller
+@Secured({"ROLE_USER","ROLE_ADMIN"})
+public class EditorController {
+
+    final Logger logger = LoggerFactory.getLogger(EditorController.class);
+
+
+    @RequestMapping(value = "editor/list", method = RequestMethod.GET)
+    public String listEditor() {
+
+        return "editor/select";
+    }
+
+
+}
