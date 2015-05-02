36beb8235217d9469ffb4f749cd652497059a055
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-05-02 10:43:35 +0200

    Add IHM + Controller

---
+++ src/main/java/edu/warbot/controllers/ContactController.java
@@ -0,0 +1,16 @@
+package edu.warbot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jimmy on 02/05/15.
+ */
+@Controller
+public class ContactController {
+
+    @RequestMapping(value = "contact")
+    public String about() {
+        return "feedback/contact";
+    }
+}
