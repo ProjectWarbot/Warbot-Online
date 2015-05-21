ecfb2272166be06a6847ac3588c33e293f3cfb9e
TsubameDono <sebastien.beugnon.pro@gmail.com>
2015-05-21 10:22:21 +0200

    Create new controllers to handle operations on members of party

--- src/main/java/edu/warbot/controllers/MemberController.java
+++ src/main/java/edu/warbot/controllers/MemberController.java
@@ -1,7 +1,10 @@
 package edu.warbot.controllers;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by beugnon on 21/05/15.
  */
+@Controller
 public class MemberController {
 }
